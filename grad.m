1adbc25e0423a35d4bcd822cb0b6815c2dc06982
andydev <akanshul97@gmail.com>
2016-10-23 22:14:45 -0400

    Updated grad function description

--- grad.m
+++ grad.m
@@ -1,14 +1,14 @@
 function g = grad(f, x, h)
 %GRAD The gradient of a function at an x
-%   grad(f,x) computes the gradient of the given function at the given x
+%   GRAD(f,x) computes the gradient of the given function at the given x
 %             (h defaults to 0.001)
 % 
-%   grad(f,x,h) computes the gradient of the given function at the guven x
+%   GRAD(f,x,h) computes the gradient of the given function at the guven x
 %               with the given h
 %
 %   Example:
 %       f = @(x) 0.5 * ( x(:,1)^2 + x(:,2)^2 )
-%       g = grad(f, [1, 4])
+%       g = GRAD(f, [1, 4])
 %       % g = [1, 4]
 
 % Default h = 0.001
